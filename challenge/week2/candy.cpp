63571050eb2f9c16c47b3445943dd872c29858d6
박지성 <kayuakana@yahoo.com>
2023-11-12 22:44:28 +0900

    issue 해결

---
+++ challenge/week2/candy.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int money;
+	int candy_price;
+
+	cout << "현재 가지고 있는 돈 : ";
+	cin >> money;
+	cout << "캔디의 가격 :";
+	cin >> candy_price;
+
+	//최대한 살 수 있는 사탕의 수
+	int n_candies = money / candy_price;
+	cout << "최대로 살 수 있는 캔디의 개수 = " << n_candies << endl;
+
+	int change = money % candy_price;
+	cout << "캔디 구입 후 남은 돈 = " << change << endl;
+	return 0;
+
+}
